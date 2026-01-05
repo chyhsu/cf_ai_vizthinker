2940046ac0ed6d5c090093b3fa3c6021ee011a66
Chun-Yuan Hsu <chunyuan.hsu.me@gmail.com>
2026-01-04 20:07:07 -0600

    refactor: Rename worker and update Durable Object migration class key in wrangler.toml.

--- backend/.wrangler/tmp/bundle-qfO98l/middleware-loader.entry.ts
+++ backend/.wrangler/tmp/bundle-qfO98l/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/home/jason/Documents/cf_ai_vizthinker/backend/.wrangler/tmp/bundle-bSXvca/middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/home/jason/Documents/cf_ai_vizthinker/backend/.wrangler/tmp/bundle-qfO98l/middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "/home/jason/Documents/cf_ai_vizthinker/backend/node_modules/wrangler/templates/middleware/common.ts";
-import type { WorkerEntrypointConstructor } from "/home/jason/Documents/cf_ai_vizthinker/backend/.wrangler/tmp/bundle-bSXvca/middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "/home/jason/Documents/cf_ai_vizthinker/backend/.wrangler/tmp/bundle-qfO98l/middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "/home/jason/Documents/cf_ai_vizthinker/backend/.wrangler/tmp/bundle-bSXvca/middleware-insertion-facade.js";
+export * from "/home/jason/Documents/cf_ai_vizthinker/backend/.wrangler/tmp/bundle-qfO98l/middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
